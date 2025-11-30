6737381c89c956111b5c1c8d69a84aa31cedc2a4
Vijay Balasubramaniam <vbalasu@gmail.com>
2025-11-29 17:53:08 -0800

    Added query parameter

--- cgi-bin/llm.py
+++ cgi-bin/llm.py
@@ -1,7 +1,13 @@
 #!/opt/cgi-python/cgi_env/bin/python
 print("Content-Type: text/plain\r\n")
-print("Asking gemma3...")
+import os
+from urllib.parse import parse_qs
 from langchain_ollama import ChatOllama
+
+query_string = os.environ.get('QUERY_STRING', '')
+params = parse_qs(query_string)
+question = params.get('q', ['What tree fits in your hand?'])[0]
+
 llm = ChatOllama(model='gemma3:12b')
-output = llm.invoke('What tree fits in your hand?').content
+output = llm.invoke(question).content
 print(output)
